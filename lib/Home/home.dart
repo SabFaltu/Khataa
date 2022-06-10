21536ffb39811223798b383b692a6eb043596a86
Skshm <58230351+Midnight1938@users.noreply.github.com>
2022-06-10 08:51:36 +0530

    final box dimension

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
 
 import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
@@ -37,56 +39,22 @@ class __ExpenseValsState extends State<_ExpenseVals> {
   int _valHuns = 2;
   int _valTens = 3;
   int _valOnes = 4;
-  int _valFinal = 0;
+
+  // valFinal = ((_valThos * 1000) + (_valHuns * 100) + (_valTens * 10) +(_valOnes));
   // Widget Vals
   final _infiLoop = true;
   final _numHapticc = true;
   final _numStyle = const TextStyle(fontSize: 31);
   final _selectNumStyle =
       const TextStyle(fontSize: 39, color: Colors.cyanAccent);
-  final _numCount = 5;
+  final _numCount = 3;
   final _numHeight = 58.0;
-  final _numWidth = 78.0; //65
+  final _numWidth = 88.0; //65
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
-              color: const Color.fromARGB(255, 48, 48, 48),
-              boxShadow: const [
-                BoxShadow(
-                  blurRadius: 20.0,
-                  offset: Offset(-5, -5),
-                  color: Color.fromARGB(255, 77, 77, 77),
-                  inset: true,
-                ),
-                BoxShadow(
-                  blurRadius: 20.0,
-                  offset: Offset(5, 5),
-                  color: Color.fromARGB(255, 19, 19, 19),
-                  inset: true,
-                ),
-              ]),
-          child: NumberPicker(
-            axis: Axis.horizontal,
-            selectedTextStyle:
-                const TextStyle(fontSize: 35, color: Colors.cyanAccent),
-            textStyle: const TextStyle(fontSize: 20),
-            itemHeight: 63,
-            itemWidth: 120,
-            itemCount: 3,
-            value: ((_valThos * 1000) +
-                (_valHuns * 100) +
-                (_valTens * 10) +
-                (_valOnes)),
-            minValue: 0,
-            maxValue: 9999,
-            onChanged: (value) => setState(() => _valFinal = value),
-          ),
-        ),
         const SizedBox(height: 40),
         Container(
           decoration: BoxDecoration(
@@ -95,13 +63,13 @@ class __ExpenseValsState extends State<_ExpenseVals> {
               boxShadow: const [
                 BoxShadow(
                   blurRadius: 20.0,
-                  offset: Offset(-11, -11),
+                  offset: Offset(-10, -10),
                   color: Color.fromARGB(255, 63, 63, 63),
                   //inset: true,
                 ),
                 BoxShadow(
                   blurRadius: 20.0,
-                  offset: Offset(11, 11),
+                  offset: Offset(10, 10),
                   color: Color.fromARGB(255, 33, 33, 33),
                   //inset: true,
                 ),
